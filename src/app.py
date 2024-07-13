3b0743ef2c53e313d8f93de77c87a146c7f7085f
Edreva <edreversmith@hotmail.com>
2024-07-13 16:09:46 +0100

    Updated styling, added more bands

--- src/app.py
+++ src/app.py
@@ -18,7 +18,9 @@ stylesheet = [
         'selector': '.genre_edge',
         'style': {
             'line-color': 'slategrey',
-            'source-arrow-shape': 'triangle'
+            'source-arrow-color': 'slategrey',
+            'source-arrow-shape': 'triangle',
+            'arrow-scale': 2
         }
     },
     {
@@ -34,7 +36,9 @@ stylesheet = [
     {
         'selector': '.band_node',
         'style': {
-            'background-color': 'slategrey',
+            # 'background-color': 'slategrey',
+            'background-fit': 'cover',
+            'background-image': 'data(url)',
             'shape': 'circle',
             'width': '50%',
             'height': '50%'
@@ -68,6 +72,7 @@ genre_nodes = [
         ('hcp', 'Hardcore Punk'),
         ('num', 'Nu Metal'),
         ('alt', 'Alt. Metal'),
+        ('sldg', 'Sludge Metal'),
               
     )
 ]
@@ -92,6 +97,7 @@ microgenre_nodes = [
         ('bg', 'Blackgaze', 'bm'),
         ('abm', 'Atmospheric Black Metal', 'bm'),
         ('bjaz','"Dark Jazz"', 'bm'),
+        ('djnt','"Djent"', 'prg'),
     )
 ]
 genre_edges = [
@@ -124,6 +130,8 @@ genre_edges = [
         ('hm', 'tm'),
         ('hm', 'alt'),
         ('im', 'num'),
+        ('doom', 'sldg'),
+        ('tm', 'gcr'),
         
     )
 ]
@@ -142,100 +150,121 @@ band_edges = [
 
 band_nodes = [
     {
-        'data': {'id': id, 'label': name, 'parent': parent, 'song': song},
+        'data': {'id': id, 'label': name, 'parent': parent, 'song': song,
+                 'url': f"https://flagcdn.com/w2560/{country}.png"},
         'classes': 'band_node'
     }
-    for id, name, parent, song in (
-        ('INF', 'In Flames', 'mdm', 'Delight and Angers'),
-        ('INS', 'Insomnium', 'mdm', 'Down With The Sun'),
-        ('ALC', 'Alcest', 'bg', 'Kodama'),
-        ('ULV', 'Ulver', 'bm', 'I Troldskog Faren Vild'),
-        ('HFS', 'Harakiri for the Sky', 'bg', 'Manifesto'),
-        ('KAK', 'Kekht Arakh', 'abm', 'Swordsman'),
-        ('ELL', 'Ellende', 'abm', 'Wind'),
-        ('MGL', 'MGLA', 'bm', 'Exercises in Futility V'),
-        ('MUL', 'My Useless Life', 'dsbm', 'A Desolate Heart'),
-        ('WHW', 'White Ward', 'bjaz', 'Cronus'),
-        ('SVB', 'Svalbard', 'bg', 'Listen to Someone'),  # Try not to die until you're dead
-        ('ELD', 'Elderwind', 'abm', 'The Magic Of Nature'), # Maybe remove
-        ('COF', 'Cult of Fire', 'abm', 'Kali Ma'), # Maybe remove
-        ('BSB', 'Black Sabbath', 'hm', 'Heaven and Hell'),
-        ('IRN', 'Iron Maiden', 'hm', 'The Trooper'),
-        ('ICE', 'Iced Earth', 'hm', 'Anthem'),
-        ('DTH', 'Death', 'dm', 'Spirit Crusher'),
-        ('RVC', 'Revocation', 'dm', 'The Grip Tightens'),
-        ('AMA', 'Amon Amarth', 'vm', 'Twilight Of The Thunder God'),  # Cry of the Blackbirds
-        ('BLK', 'Be\'lakor', 'pmdm', 'Abeyance'),  # Root to Sever, Smoke of many fires
-        ('SWK', 'Soilwork', 'mdm', 'Distortion Sleep'),
-        ('DTQ', 'Dark Tranquility', 'mdm', 'Time out of Place'),
-        ('OBC', 'Orbit Culture', 'mdm', 'See Through Me'),
-        ('HNT', 'The Haunted', 'mdm', 'The Fallout'),
-        ('DIS', 'Disarmonia Mundi', 'mdm', 'Celestial Furnace'),
-        ('UTA', 'Unleash the Archers', 'pm', 'Apex'),
-        ('TRI', 'Triosphere', 'pm', 'Steal Away the Light'),
-        ('ODO', 'Orden Ogan', 'pm', 'Gunman'),
-        ('PWF', 'Powerwolf', 'pm', 'Army of the Night'),
-        ('OPT', 'Opeth', 'prg', 'Reverie / Harlequin Forest'),
-        ('DRT', 'Dream Theater', 'prg', 'Pull Me Under'),
-        ('TVM', 'Trivium', 'mcr', 'The Sin and the Sentence'),
-        ('ALD', 'As I Lay Dying', 'mcr', 'Through Struggle'),
-        ('LMK', 'Landmvrks', 'mcr', 'Creature'),
-        ('AVA', 'Aviana', 'dcr', 'Oblivion'),
-        ('ALT', 'Allt', 'pmcr', 'Remnant'),
-        ('ARC', 'Architects', 'mcr', 'Dead Man Talking'),
-        ('THN', 'Thornhill', 'mcr', 'Human'),
-        ('BRT', 'Bury Tomorow', 'mcr', 'The Agonist'),
-        ('MNM', 'Monuments', 'pmcr', 'False Providence'),
-        ('ERA', 'ERRA', 'pmcr', 'Nigh to Silence'),
-        ('NRT', 'Northlane', 'pmcr', '4D'),
-        ('SPB', 'Spiritbox', 'pmcr', 'The Beauty of Suffering'),
-        ('INA', 'Invent Animate', 'pmcr', 'Purity Weeps'),
-        ('SIP', 'Silent Planet', 'pmcr', 'Second Sun'),
-        ('CUR', 'Currents', 'pmcr', 'The Place Where I Feel Safest'),
-        ('ECB', 'Electric Callboy', 'ecr', 'Hypa Hypa'),
-        ('ESH', 'Enter Shikari', 'ecr', '...Meltdown'),
-        ('SLP', 'Slipknot', 'num', 'Wait and Bleed'),
-        ('MAC', 'Machine Head', 'num', 'Circle the Drain'),
-        ('LIP', 'Linkin Park', 'num', 'Don\'t Stay'),
-        ('SOD', 'System of a Down', 'num', 'B.Y.O.B'),
-        ('WIT', 'Within Temptation', 'pop', 'Bleed Out'),
-        ('BCH', 'Blind Channel', 'pop', 'Flatline'),
-        # Bad Omens
-        ('BMT', 'Bring Me The Horizon', 'pop', 'Doomed'),
-        ('FFA', 'Fit for an Autopsy', 'dcr', 'Two Towers'),
-        ('WCH', 'Whitechapel', 'dcr', 'A Bloodsoaked Symphony'),
-        ('SOI', 'Shadow of Intent', 'pdcr', 'Intensified Genocide'),
-        # ('DWP', 'The Devil Wears Prada', 'mcr', )
-        ('HLB', 'Humanity\'s Last Breath', 'pdcr', 'Instill'), # Blackened Deathcore with Thall
-        ('VLJ', 'Vildjharta', 'thl', 'lavender haze'),
-        ('MIR', 'Mirar', 'thl', 'Franka'),
-        ('MGD', 'Megadeth', 'tm', 'The Scorpion'),
-        ('MCH', 'Melechech', 'tm', 'Multiple Truths'),  # blackened thrash metal
-        ('MET', 'Metallica', 'tm', 'The Unforgiven'),
-        ('STS', 'Swallow the Sun', 'doom', 'Falling World'),  # blackened death-doom with gothic edge
-        ('FAE', 'Faetooth', 'doom', 'Strange Ways'),  # Fairy doom
-        ('MDB', 'My Dying Bride', 'doom', 'Your Broken Shore'),
-        ('FOE', 'Fall of Efrafa', 'crst', 'Pity the Weak'),
-        ('LAC', 'Lacuna Coil', 'goth', 'Our Truth'),
-        ('PRD', 'Paradise Lost', 'goth', 'Ghosts'),
-        ('KTA', 'Katatonia', 'goth', 'For My Demons'),
-        ('CAT', 'Cattle Decapitation', 'gcr', 'Manufactured Extinct'),
-        ('RAM', 'Rammstein', 'im', 'Feuer Frei!'),
-        ('RNC', 'Raunchy', 'im', 'A Heavy Burden'),
-        ('ELV', 'Eluveitie', 'celt', 'Quoth the Raven'),
-        ('GRA', 'Grai', 'fm', 'Song of Dead Water'),
-        ('KPK', 'Korpiklaani', 'fm', 'Ruumiinmultaa'),
-        ('ALE', 'Alestorm', 'pirt', 'Keelhauled'),
-        ('NWS', 'Nightwish', 'sm', 'Ghost Love Score'),
-        ('AVT', 'Avantasia', 'sm', 'The Scarecrow'),  # Metal opera
-        ('LOG', 'Lamb of God', 'gm', 'Memento Mori'),
-        ('GOJ', 'Gojira', 'pgm', 'The Axe'),  # Prog/Groove/Death
-        ('JIN', 'Jinjer', 'pgm', 'On The Top'),
-        ('TOO', 'Tool', 'prg', 'Lateralus'),
-        ('DFT', 'Deftones', 'alt', 'Digital Bath'),
-        ('SLT', 'Sleep Token', 'alt', 'Alkaline'),
-        ('ZAA', 'Zeal and Ardor', 'alt', 'Gotterdamerung'),
-        ('NOK', 'Knocked Loose', 'hcp', 'Deep in the Willow'),
+    for id, name, parent, song, country in (
+        ('INF', 'In Flames', 'mdm', 'Delight and Angers','se'),
+        ('INS', 'Insomnium', 'mdm', 'Down With The Sun', 'fi'),
+        ('ALC', 'Alcest', 'bg', 'Kodama', 'fr'),
+        ('ULV', 'Ulver', 'bm', 'I Troldskog Faren Vild', 'no'),
+        ('HFS', 'Harakiri for the Sky', 'bg', 'Manifesto', 'au'),
+        ('KAK', 'Kekht Arakh', 'abm', 'Swordsman', 'fi'),
+        ('ELL', 'Ellende', 'abm', 'Wind', 'de'),
+        ('MGL', 'MGLA', 'bm', 'Exercises in Futility V', 'pl'),
+        ('MUL', 'My Useless Life', 'dsbm', 'A Desolate Heart', 'us'),
+        ('WHW', 'White Ward', 'bjaz', 'Cronus', 'ua'),
+        ('SVB', 'Svalbard', 'bg', 'Listen to Someone', 'ca'),  # Try not to die until you're dead
+        ('ELD', 'Elderwind', 'abm', 'The Magic Of Nature', 'ru'), # Maybe remove
+        ('COF', 'Cult of Fire', 'abm', 'Kali Ma', 'cz'), # Maybe remove
+        ('BSB', 'Black Sabbath', 'hm', 'Heaven and Hell', 'gb'),
+        ('IRN', 'Iron Maiden', 'hm', 'The Trooper', 'gb'),
+        ('ICE', 'Iced Earth', 'hm', 'Anthem', 'us'),
+        ('DTH', 'Death', 'dm', 'Spirit Crusher', 'us'),
+        ('SEP', 'Septicflesh', 'dm', 'Anubis', 'gr'),
+        ('RVC', 'Revocation', 'dm', 'The Grip Tightens', 'us'),
+        ('AMA', 'Amon Amarth', 'vm', 'Twilight Of The Thunder God', 'no'),  # Cry of the Blackbirds
+        ('BLK', 'Be\'lakor', 'pmdm', 'Abeyance', 'au'),  # Root to Sever, Smoke of many fires
+        ('SWK', 'Soilwork', 'mdm', 'Distortion Sleep', 'se'),
+        ('DTQ', 'Dark Tranquility', 'mdm', 'Time out of Place', 'se'),
+        ('OBC', 'Orbit Culture', 'mdm', 'See Through Me', 'se'),
+        ('HNT', 'The Haunted', 'mdm', 'The Fallout', 'se'),
+        ('DIS', 'Disarmonia Mundi', 'mdm', 'Celestial Furnace', 'it'),
+        ('UTA', 'Unleash the Archers', 'pm', 'Apex', 'ca'),
+        ('TRI', 'Triosphere', 'pm', 'Steal Away the Light', 'no'),
+        ('ODO', 'Orden Ogan', 'pm', 'Gunman', 'de'),
+        ('PWF', 'Powerwolf', 'pm', 'Army of the Night', 'de'),
+        ('OPT', 'Opeth', 'prg', 'Reverie / Harlequin Forest', 'se'),
+        ('CAL', 'Caligulas Horse', 'prg', 'The World Breathes With me', 'au'),
+        ('DRT', 'Dream Theater', 'prg', 'Pull Me Under', 'us'),
+        ('TVM', 'Trivium', 'mcr', 'The Sin and the Sentence', 'us'),
+        ('ALD', 'As I Lay Dying', 'mcr', 'Through Struggle', 'us'),
+        ('LMK', 'Landmvrks', 'mcr', 'Creature', 'fr'),
+        ('AVA', 'Aviana', 'dcr', 'Oblivion', 'se'),
+        ('ALT', 'Allt', 'pmcr', 'Remnant', 'se'),
+        ('ARC', 'Architects', 'mcr', 'Dead Man Talking', 'gb'),
+        ('PWD', 'Parkway Drive', 'mcr', 'Carrion', 'au'),
+        ('THN', 'Thornhill', 'mcr', 'Human', 'au'),
+        ('BRT', 'Bury Tomorow', 'mcr', 'The Agonist', 'gb'),
+        ('CRY', 'Crystal Lake', 'mcr', 'Curse', 'jp'),
+        ('MNM', 'Monuments', 'djnt', 'False Providence', 'gb'),
+        ('ERA', 'ERRA', 'pmcr', 'Nigh to Silence', 'us'),
+        ('NRT', 'Northlane', 'pmcr', '4D', 'au'),
+        ('SPB', 'Spiritbox', 'pmcr', 'The Beauty of Suffering', 'ca'),
+        ('INA', 'Invent Animate', 'pmcr', 'Purity Weeps', 'us'),
+        ('SIP', 'Silent Planet', 'pmcr', 'Second Sun', 'us'),
+        ('CUR', 'Currents', 'pmcr', 'The Place Where I Feel Safest', 'us'),
+        ('ECB', 'Electric Callboy', 'ecr', 'Hypa Hypa', 'de'),
+        ('ESH', 'Enter Shikari', 'ecr', '...Meltdown', 'gb'),
+        ('CRS', 'Crossfaith', 'ecr', 'ZERO', 'jp'),
+        ('BSC', 'Blood Stain Child', 'ecr', 'Eternal', 'jp'),
+        ('NOO', 'Noosphera', 'ecr', 'Reflejos', 'mx'),
+        ('SLP', 'Slipknot', 'num', 'Wait and Bleed', 'us'),
+        ('MAC', 'Machine Head', 'num', 'Circle the Drain', 'us'),
+        ('LIP', 'Linkin Park', 'num', 'Don\'t Stay', 'us'),
+        ('BLW', 'Bloodywood', 'num', 'Machi Bhasad', 'in'),
+        ('SOD', 'System of a Down', 'num', 'B.Y.O.B', 'am'),
+        ('WIT', 'Within Temptation', 'pop', 'Bleed Out', 'nl'),
+        ('BCH', 'Blind Channel', 'pop', 'Flatline', 'fi'),
+        ('BAD', 'Bad Omens', 'pop', 'Artificial Suicide', 'us'),
+        ('BMT', 'Bring Me The Horizon', 'pop', 'Doomed', 'gb'),
+        ('FFA', 'Fit for an Autopsy', 'dcr', 'Two Towers', 'us'),
+        ('WCH', 'Whitechapel', 'dcr', 'A Bloodsoaked Symphony', 'us'),
+        ('SOI', 'Shadow of Intent', 'pdcr', 'Intensified Genocide', 'us'),
+        ('DWP', 'The Devil Wears Prada', 'pop', 'Broken', 'us'),
+        ('ALP', 'Alpha Wolf', 'mcr', 'Bleed 4 U', 'au'),
+        ('VOV', 'Void of Vision', 'mcr', 'Empty', 'au'),
+        ('FUT', 'Future Palace', 'mcr', 'Malphas', 'de'),
+        ('BOU', 'Boundaries', 'mcr', 'Inhale the Grief', 'us'),
+        ('HLB', 'Humanity\'s Last Breath', 'pdcr', 'Instill', 'se'), # Blackened Deathcore with Thall
+        ('VLJ', 'Vildjharta', 'thl', 'lavender haze', 'se'),
+        ('MIR', 'Mirar', 'thl', 'Franka', 'fr'),
+        ('MGD', 'Megadeth', 'tm', 'The Scorpion', 'us'),
+        ('MCH', 'Melechech', 'tm', 'Multiple Truths', 'il'),  # blackened thrash metal
+        ('MET', 'Metallica', 'tm', 'The Unforgiven', 'us'),
+        ('STS', 'Swallow the Sun', 'doom', 'Falling World', 'fi'),  # blackened death-doom with gothic edge
+        ('FAE', 'Faetooth', 'doom', 'Strange Ways', 'us'),  # Fairy doom
+        ('MDB', 'My Dying Bride', 'doom', 'Your Broken Shore', 'gb'),
+        ('FOE', 'Fall of Efrafa', 'crst', 'Pity the Weak', 'gb'),
+        ('MAR', 'Marno', 'crst', 'Samota', 'cz'),
+        ('LAC', 'Lacuna Coil', 'goth', 'Our Truth', 'it'),
+        ('PRD', 'Paradise Lost', 'goth', 'Ghosts', 'gb'),
+        ('KTA', 'Katatonia', 'goth', 'For My Demons', 'se'),
+        ('CAT', 'Cattle Decapitation', 'gcr', 'Manufactured Extinct', 'us'),
+        ('RAM', 'Rammstein', 'im', 'Feuer Frei!', 'de'),
+        ('RNC', 'Raunchy', 'im', 'A Heavy Burden','dk'),
+        ('ELV', 'Eluveitie', 'celt', 'Quoth the Raven', 'ch'),
+        ('GRA', 'Grai', 'fm', 'Song of Dead Water', 'ru'),
+        ('KPK', 'Korpiklaani', 'fm', 'Ruumiinmultaa', 'fi'),
+        ('TEN', 'Tengger Cavalry', 'fm', 'Ride Into Grave and Glory', 'cn'),
+        ('ALE', 'Alestorm', 'pirt', 'Keelhauled', 'gb'),
+        ('NWS', 'Nightwish', 'sm', 'Ghost Love Score', 'fi'),
+        ('AVT', 'Avantasia', 'sm', 'The Scarecrow', 'de'),  # Metal opera
+        ('LOG', 'Lamb of God', 'gm', 'Memento Mori', 'us'),
+        ('AWE', 'Alien Weaponry', 'gm', 'Ahi Ka', 'nz'),
+        ('SPL', 'Sepultura', 'gm', 'Roots Bloody Roots', 'br'),
+        ('GOJ', 'Gojira', 'pgm', 'The Axe', 'fr'),  # Prog/Groove/Death
+        ('JIN', 'Jinjer', 'pgm', 'On The Top', 'ua'),
+        ('TOO', 'Tool', 'prg', 'Lateralus', 'us'),
+        ('DFT', 'Deftones', 'alt', 'Digital Bath', 'us'),
+        ('LOA', 'Loathe', 'alt', 'Screaming', 'gb'),
+        ('SLT', 'Sleep Token', 'alt', 'Alkaline', 'gb'),
+        ('ZAA', 'Zeal and Ardor', 'alt', 'Gotterdamerung', 'ch'),
+        ('NOK', 'Knocked Loose', 'hcp', 'Deep in the Willow', 'us'),
+        ('BWA', 'Beastwars', 'sldg', 'Damn the Sky', 'nz'),
+        ('MAS', 'Mastodon', 'sldg', 'The Motherload', 'us'),
+        ('MES', 'Meshuggah', 'djnt', 'Bleed', 'se'),
         
     )
 ]
@@ -269,5 +298,5 @@ def navigate_to_url(node_data):
 
     
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port="8050")
-    # app.run(debug=True)
+    # app.run_server(host="0.0.0.0", port="8050")
+    app.run(debug=True)
